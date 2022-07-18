57721c54ece184b6a8da5f00bf2a89aff3412621
M_2171923 <eric.florence@cognizant.com>
2022-07-18 14:55:37 -0500

    refactored restaurant screen, styles, setting up navigation

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { RestaurantsScreen } from './src/features/restaurants/screens/restaurants.screen';
-import { ThemeProvider } from 'styled-components/native';
+import { ThemeProvider } from 'styled-components';
 import { theme } from './src/infrastructure/theme';
 import { useFonts as useOswald, Oswald_400Regular } from '@expo-google-fonts/oswald';
 import { useFonts as useLato, Lato_400Regular } from '@expo-google-fonts/lato';
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import styled from "styled-components";
-import { View, SafeAreaView, StatusBar } from 'react-native';
+import { View, SafeAreaView, StatusBar, FlatList } from 'react-native';
 import { Searchbar } from 'react-native-paper';
 import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
+import { Spacer } from '../../../components/spacer/spacer.component';
 
+
+export const RestaurantsScreen = () => (
+
+    <SafeView>
+        <SearchContainer>
+            <Searchbar />
+        </SearchContainer>
+        <RestaurantList 
+            data={[{ name: 1 }, { name: 2 }, { name: 3 }, { name: 4 }]} 
+            renderItem={() => 
+                <Spacer position="bottom" size="large">
+                    <RestaurantInfoCard />
+                </Spacer>} 
+            keyExtractor={(item) => item.name} />
+    </SafeView>
+);
+
+// STYLES *********************************************************************************************************************
 
 const SafeView = styled(SafeAreaView)`
     flex: 1;
@@ -14,20 +33,9 @@ const SearchContainer = styled(View)`
     padding: ${(props) => props.theme.space[3]};
 `;
 
-const RestaurantListContainer = styled(View)`
-    flex: 1;
-    padding: ${(props) => props.theme.space[3]};
-`;
+const RestaurantList = styled(FlatList).attrs({
+    contentContainerStyle: {
+        padding: 16,
+    }
+})``;
 
-
-export const RestaurantsScreen = () => (
-
-    <SafeView>
-        <SearchContainer>
-            <Searchbar />
-        </SearchContainer>
-        <RestaurantListContainer>
-            <RestaurantInfoCard />
-        </RestaurantListContainer>
-    </SafeView>
-);
